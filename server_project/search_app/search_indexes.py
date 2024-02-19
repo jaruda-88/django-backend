9b6b22935247184c6a1c3e890c1acce25bceaa46
jaruda-88 <jaruda88@gmail.com>
2024-02-19 19:19:27 +0900

    update

--- server_project/search_app/search_indexes.py
+++ server_project/search_app/search_indexes.py
@@ -3,5 +3,12 @@ from django_elasticsearch_dsl.registries import registry
 from .models import Article, ArticleDocument
 
 
-# @registry.register_document
-# class Arti
+@registry.register_document
+class ArticleDocument(Document):
+    class Index:
+        name = 'articles'
+        settings = {'number_of_shards' : 1, 'number_of_replicas': 0}
+
+    class Django:
+        model = Article
+        fields = ['title', 'content']
--- server_project/search_app/views.py
+++ server_project/search_app/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from django.http import JsonResponse
+from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer
 from elasticsearch_dsl import connections, Q
 
 
-def search(request)
+class ArticleListCreateAPIVIEW(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+# def search(request):
+#     query = request.GET.get('query', '')
+
+#     s = Article.search().queyr(
+#         Q('multi_match', query=query, fields=["title", "content"])
+#     )
+
+#     response = s.execute()
+#     articles = [hit.to.dict() for hit in response]
+
+#     return JsonResponse(articles, safe=False)
+
+
 # class ArticleView(viewsets.ModelViewSet):
 #     queryset = Article.objects.all()
 #     serializer_class = ArticleSerializer
 
-    # def get_queryset(self):
-    #     title = self.request.query_params.get("title")
-    #     category = self.request.query_params.get("category")
+#     def get_queryset(self):
+#         title = self.request.query_params.get("title")
+#         category = self.request.query_params.get("category")
 
-    #     query = Q()
+#         query = Q()
 
-    #     if title:
-    #         query &= Q(title=title)
-    #     if category:
-    #         query &= Q(category=category)
+#         if title:
+#             query &= Q(title=title)
+#         if category:
+#             query &= Q(category=category)
         
-    #     queryset = Article.objects.filter(query)
+#         queryset = Article.objects.filter(query)
 
-    #     return queryset
+#         return queryset
 
